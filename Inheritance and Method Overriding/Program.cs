cbe11de4fbd38c150cd7eb6f51a131fb14f518e4
Biadela Eric Deklu <bedeklu@st.ug,edu.gh>
2025-08-06 01:22:02 +0000

    Added Inheritance and Method Overriding task

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_and_Method_Overriding
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal generic = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            generic.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
